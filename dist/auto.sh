4bc231e5940d42bd15c32c5c1dc4e4158bdc02b7
Lee Donggeon <inspirit941@gmail.com>
2018-11-04 13:13:19 +0900

    ViewDataCreate

---
+++ dist/auto.sh
@@ -0,0 +1,11 @@
+composer archive create -t dir -n ../
+
+composer network install -a hlf_kisa@0.0.1.bna -c PeerAdmin@hlfv1
+
+composer network start -c PeerAdmin@hlfv1 -n hlf_kisa -V 0.0.1 -A admin -S adminpw
+
+composer card delete -c admin@hlf_kisa
+
+composer card import -f admin@hlf_kisa.card
+
+composer-rest-server -c admin@hlf_kisa -n never -d n
