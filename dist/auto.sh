9e9cd67af5749a746a0db989a0a871bac9966400
Lee Donggeon <inspirit941@gmail.com>
2018-11-04 18:45:46 +0900

    query

--- dist/auto.sh
+++ dist/auto.sh
@@ -4,9 +4,9 @@ cd ../../
 ./startFabric.sh
 cd hlf_kisa/dist/
 
-composer network install -a hlf_kisa@0.0.6.bna -c PeerAdmin@hlfv1
+composer network install -a hlf_kisa@0.0.1.bna -c PeerAdmin@hlfv1
 
-composer network start -c PeerAdmin@hlfv1 -n hlf_kisa -V 0.0.6 -A admin -S adminpw
+composer network start -c PeerAdmin@hlfv1 -n hlf_kisa -V 0.0.1 -A admin -S adminpw
 
 composer card delete -c admin@hlf_kisa
 
