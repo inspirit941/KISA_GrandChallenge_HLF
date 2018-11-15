ba68a3a6a9603082e749f3f243623f838dc52130
Lee Donggeon <inspirit941@gmail.com>
2018-11-15 11:34:13 +0900

    Viewpermin fix

--- dist/auto.sh
+++ dist/auto.sh
@@ -1,12 +1,12 @@
 composer archive create -t dir -n ../
 
-# cd ../../
-# ./startFabric.sh
-# cd hlf_kisa/dist/
+cd ../../
+./startFabric.sh
+cd hlf_kisa/dist/
 
-composer network install -a hlf_kisa@0.0.1.bna -c PeerAdmin@hlfv1
+composer network install -a hlf_kisa@0.0.9.bna -c PeerAdmin@hlfv1
 
-composer network start -c PeerAdmin@hlfv1 -n hlf_kisa -V 0.0.1 -A admin -S adminpw
+composer network start -c PeerAdmin@hlfv1 -n hlf_kisa -V 0.0.9 -A admin -S adminpw
 
 composer card delete -c admin@hlf_kisa
 
